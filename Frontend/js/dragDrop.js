97a1a2eb903f7cabdf9e6147df41916966b385af
ARS-TECNOLOGIC <anselmoroberto003@gmail.com>
2025-04-23 16:57:00 -0300

    Drag Drop

--- Frontend/js/dragDrop.js
+++ Frontend/js/dragDrop.js
@@ -20,8 +20,11 @@ const drop = (event) => {
    if(event.target.classList.contains('column')){
        const id = event.dataTransfer.getData("text");
        const card = document.querySelector(`#${id}`);
+       card.classList.remove(card.classList[1]);
        event.target.appendChild(card);
-       event.target.classList.remove('column--enter')
+       event.target.classList.remove('column--enter');
+       const classDrop = event.target.classList[1];
+       card.classList.add(classDrop);
    }
 }
 
@@ -51,19 +54,36 @@ const createdElement = (tag)=>{
 const ListCard = async ()=>{
     const tasks =await listTasks();
     tasks.forEach((task)=>{
-        const {id, title, status} = task;
+        const {id, title, status, created_at} = task;
         
+        const classStatus = (status === "em andamento") ? "andamento":status;
+
         const card = createdElement("div");
         card.draggable=true;
         card.id="card-"+id;
         card.classList.add("card");
+        card.classList.add(classStatus);
 
-        const spanNome = createdElement("span");
-        spanNome.innerText =title;
-        card.appendChild(spanNome);
+        const titulo = createdElement("h2");
+        titulo.innerText =title;
+        card.appendChild(titulo);
+        
+        const spanStatus = createdElement("span");
+        spanStatus.classList.add("status");
+        spanStatus.id="status-"+id;
+        spanStatus.innerText = status.toUpperCase();
+        card.appendChild(spanStatus);
 
+        const createdAt = createdElement("div");
+        createdAt.classList.add("created-at");
+        const date = new Date(created_at).toLocaleString('pt-br', {dateStyle: 'short', timeStyle: 'short'});
+        createdAt.innerText = date;
+        card.appendChild(createdAt);
+        
         card.addEventListener("dragstart",dragStart);
-        const column = document.querySelector('#col1');
+        const column = document.querySelector(`.column.${classStatus}`);
+        
+        
         column.appendChild(card);
     })
 }
